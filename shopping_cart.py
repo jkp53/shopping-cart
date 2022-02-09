38c1e034b7a36d6ac2a2071ebbd5fb6535d8a236
jkp53 <80708001+jkp53@users.noreply.github.com>
2022-02-09 17:59:28 -0500

    setup

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,5 +1,3 @@
-# shopping_cart.py
-
 products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
     {"id":2, "name": "All-Seasons Salt", "department": "pantry", "aisle": "spices seasonings", "price": 4.99},
